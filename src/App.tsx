7e60edc3c49e2ad1c70699e2401852a99ba3f766
Yumin22224 <yumin224@snu.ac.kr>
2024-03-24 07:18:18 +0900

    댓글

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { useEffect, useState } from "react";
 import { auth } from "./routes/firebase";
 import Layout from "./routes/NavBar";
 import FirstPage from "./routes/Login/firstpage";
+import Home from "./routes/Home/HomePage";
+import Profile from "./routes/Profile/ProfilePage";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -26,14 +28,6 @@ function App() {
         <>Loading</>
       ) : (
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={<Layout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/firstpage" element={<FirstPage />} />
-            <Route path="/create-account" element={<CreateAccount />} />
-            <Route path="/workspace" element={<WorkSpace />} />
-            <Route path="/whiteboard" element={<WhiteBoard />} />
-=======
           <Route path="/login" element={<Login />} />
           <Route path="/create-account" element={<CreateAccount />} />
           <Route path="/" element={<Layout />}>
@@ -41,7 +35,6 @@ function App() {
             <Route path="/workspace" element={<WorkSpace />} />
             <Route path="/whiteboard" element={<WhiteBoard />} />
             <Route path="/profile" element={<Profile />} />
->>>>>>> 8e3f93cb3240748febabba65367844ec6f826258
             <Route path="/" element={<ProtectedRoute />}>
               <Route path="/find-family" element={<FindFamily />} />
             </Route>
