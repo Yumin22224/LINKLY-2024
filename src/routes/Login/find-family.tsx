e5a08d644bad05d50aec81f8a0e86677560fcac6
Leafguyk <81000589+Leafguyk@users.noreply.github.com>
2024-03-23 21:13:00 +0900

    가족 찾기 추가

--- src/routes/Login/find-family.tsx
+++ src/routes/Login/find-family.tsx
@@ -1,43 +1,64 @@
 import styled from "styled-components";
 import { auth, db } from "../firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
 import { useState } from "react";
 
 const Form = styled.form``;
 const Input = styled.input``;
-const CreateFamilyBtn = styled.button``;
 
 export default function FindFamily() {
-  const [familyId, setFamilyId] = useState(undefined);
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { name, value },
-    } = e;
-    if (name === "familyId") {
-      setFamilyId(value);
-    }
-  };
+  const [familyId, setFamilyId] = useState("");
+  const [familyName, setFamilyName] = useState("");
+  const [familyIds, setFamilyIds] = useState<string[]>([]);
+  const fetchFamilies  =async () => {
+    const tweetsQuery = query(collection(db,"tweets"));
+    await onSnapshot(tweetsQuery, (snapshot) => {
+        setFamilyIds(snapshot.docs.map((doc) => doc.id));
+    })}
   const onSubmit = async () => {
+    const user = auth.currentUser;
+    await fetchFamilies();
+    if (user === null || familyIds.includes(familyId)) return;
     await addDoc(collection(db, "users"), {
-      id: auth.currentUser.uid,
+      id: user.uid,
       familyId: familyId,
     });
   };
-  const onClick = async () => {
-    
+  const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const {target:{name,value}} = e;
+    if (name === "familyId") {
+        setFamilyId(value);
+    } else if (name === "familyName") {
+      setFamilyName(value);
+    }}
+  const onCreate = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (user === null) return;
+    const doc = await addDoc(collection(db, "families"), {
+      familyName: familyName,
+      memberList: [user.uid]
+    });
+    await addDoc(collection(db, "users"), {
+      id: user.uid,
+      familyId: doc.id,
+    });
   };
   return (
     <>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
-          type="number"
+          type="text"
           placeholder="Family ID"
           value={familyId}
           name="familyId"
         ></Input>
       </Form>
-      <CreateFamilyBtn onClick={onClick}>Create Family</CreateFamilyBtn>
+      <Form onSubmit={onCreate}>
+        <Input onChange={onChange} type="text" value={familyName} name="familyName" ></Input>
+        <Input type="submit">Create Family</Input>
+      </Form>
     </>
   );
 }
